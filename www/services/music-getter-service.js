84e68568282fb33bf4568c6c5f4563a3e340eb88
paulief <paulfagan1991@gmail.com>
2015-07-08 22:07:20 -0400

    Added getting playlists

--- www/services/music-getter-service.js
+++ www/services/music-getter-service.js
@@ -7,21 +7,41 @@ services.factory('MusicGetter', ['$q', '$http', function($q, $http) {
 			case "soundcloud":
 				switch(tracklistType) {
 					case "favorites":
-						var retProm = this.getSoundcloudFavorites(); 
-						console.log(retProm);
-						return retProm;
-						break;
+						return this.getSoundcloudFavorites(); 
+					break;
 
 
 				}
-				break;
+			break;
 			default:
 		}
 	};
+
+	musicGetterService.getPlaylists = function(source) {
+		console.log("getPlaylists called");
+		switch (source) {
+			case "soundcloud":
+				return this.getSoundcloudPlaylists();
+			break;
+		};
+	};
+
 	musicGetterService.getSoundcloudFavorites = function() {
 		console.log("called getSoundcloudFavorites");
 		var deferred = $q.defer();
 		$http.get('http://api.soundcloud.com/users/70188989/favorites?client_id=' + SOUNDCLOUD_CLIENT_ID).success(function(data, status, headers, config) {
+			deferred.resolve(data);
+		}).error(function(data, status, headers, config) {
+			console.log("error in GET request");
+		});
+
+		return deferred.promise;
+	};
+
+	musicGetterService.getSoundcloudPlaylists = function() {
+		console.log("called getSoundcloudPlaylists");
+		var deferred = $q.defer();
+		$http.get('http://api.soundcloud.com/users/70188989/playlists?client_id=' + SOUNDCLOUD_CLIENT_ID).success(function(data, status, headers, config) {
 			deferred.resolve(data);
 		}).error(function(data, status, headers, config) {
 			console.log("error in GET request");
